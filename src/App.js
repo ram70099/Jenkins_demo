93f1fe1470af04e1b003d624c6b440453ca9f88c
ram <ramrattan099@gmail.com>
2025-04-22 11:42:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import MyComponent from './Header/Header'; // Import the MyComponent
 const App = () => {
   return (
     <div>
-      <h1>Welcome to my site</h1>
-      <h1>Welcome to my site</h1>
+    
       <MyComponent /> {/* Use MyComponent here */}
     </div>
   );
